b0ce6bf5d3d93f23de91dd5e1a5b93bb3ec1d1aa
andrefakhoury <andreluis1704@gmail.com>
2019-06-28 20:37:10 -0300

    update final

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
-# org-arquiz
+# Org ArQUIZ
+
 Quiz sobre Organização e Arquitetura de Computadores
+
+Foi desenvolvido um Recurso Educacional Aberto - um quiz web educativo sobre os conteúdos da disciplina SSC0902 - Organização e Arquitetura de Computadores.
+
+O quiz fornece ao jogador 10 perguntas de múltipla escolha, as quais são escolhidas aleatoriamente entre as diversas questões disponíveis. Após responder as questões é mostrado ao jogador sua pontuação final e uma mensagem informando se ele seria aprovado ou não na disciplina.
+
+## Desenvolvedores
+
+- André Luís Mendes Fakhoury
+- David Cairuz
+- Gustavo Soares
+- Thiago Preischadt
+
+Matéria: SSC0902 – Organização e Arquitetura de Computadores
+
+Docente: Sarita Mazzini Bruschi
+
+## Desenvolvimento
+
+No desenvolvimento do REA foram utilizados uma página html e javascript. O script faz a seleção das questões que serão exibidas e processa os acertos e erros do usuário. Também foi utilizada a plataforma Github no desenvolvimento, que foi essencial tanto para o controle de versão quanto para disponibilizar a aplicação ao público.
+
+Já as questões utilizadas no quiz foram propostas pelo próprio grupo, usando como base os conhecimentos próprios, os slides e o livro-texto da disciplina.
+
+## Sobre o sistema
+
+Ao abrir o site, basta clicar em “Começar” para começar um novo jogo. Após isso, serão selecionadas (aleatoriamente) dez questões de um banco de questões. Para respondê-las, basta selecionar a resposta que julgar correta e clicar em “Responder”.
+
+Conforme as questões são respondidas, a pontuação se atualiza. Ao final de 10 questões é mostrada a pontuação final.
+
+## Conclusão
+
+Com o desenvolvimento de um Recurso Educacional Aberto, pode ser feito um sistema com fins didáticos, que pode ser utilizado (ou até mesmo melhorado) por outras pessoas.
+
+Para estender o recurso, são propostas algumas melhorias, como a ampliação do conteúdo abordado (devido a forma que o projeto foi estruturado, novas questões sobre outros temas podem ser facilmente adicionadas), e extensão para outras disciplinas (o quiz pode ser adaptado para outras disciplinas, o que pode ser feito pela adição de uma opção de selecionar disciplina como pela troca do banco de questões).
+
+## Referências bibliográficas
+
+PATTERSON, D. A., HENNESSY, J.L. Computer Organization and Design, Fifth Edition: The Hardware/Software Interface.
--- script.js
+++ script.js
@@ -18,10 +18,14 @@ function shuffle(array) {
 	return array;
 }
 
+// variaveis auxiliares, com informacoes das questoes
 var questoes, ordem;
 var curQuestao, pontos, qttQuestao;
 
+/** Comeca um novo jogo, atualizando as informacoes necessarias */
 function comecarJogo() {
+
+	// insere as questoes do banco, e randomiza a ordem
 	questoes = data;
 
 	ordem = [];
@@ -32,56 +36,65 @@ function comecarJogo() {
 	ordem = shuffle(ordem);
 
 	pontos = 0;
-	qttQuestao = Math.min(questoes.length, 10);
+	qttQuestao = Math.min(questoes.length, 10); // limita a quantidade de questoes
 
+	// deixa as questoes visiveis, e o botao de iniciar invisivel
 	document.getElementById("container").style.visibility = "visible";
 	document.getElementById("iniciar").style.visibility = "hidden";
 
+	// insere a base das questoes e pontuacao
 	document.getElementById("container").innerHTML = '<div id="pontuacao">Pontos: 0</div>'+
 													 '<div id="questao" class="questao"></div>';
 
 	document.getElementById("pontuacao").innerHTML = "Pontos: " + pontos;
 
+	// chama a primeira pergunta
 	curQuestao = -1;
 	perguntar();
 }
 
+/** Encerra o jogo atual */
 function encerrar() {
 	var elem = document.getElementById("questao");
 	elem.innerHTML = '<b>Acabou!</b> Voce fez <b>' + pontos + ' pontos</b>, de ' + qttQuestao + ' possíveis.<hr id="final">';
 
 	var porcentagem = pontos/qttQuestao;
 
+	// muda a reacao do final, dependendo dos acertos
 	if (porcentagem == 1) {
-		elem.innerHTML += "O cara é bom! Gabaritou tudin :)";
+		elem.innerHTML += "O cara é bom! Gabaritou tudo :)";
 	} else if (porcentagem >= 0.5) {
-		elem.innerHTML += "Show, passou sem rec!";
+		elem.innerHTML += "Show, passou sem recuperação!";
 	} else if (porcentagem >= 0.3) {
-		elem.innerHTML += "Quebrou o pré, mas dava pra ter ido melhor..."
+		elem.innerHTML += "Quebrou o pré, mas vai precisar fazer a recuperação..."
 	} else {
-		elem.innerHTML += "Tá reprovado, vai ter q fazer de novo kk";
+		elem.innerHTML += "Você foi reprovado :(";
 	}
 
+	// insere o botao de reiniciar jogo
 	elem.innerHTML += '<br><button onclick="comecarJogo();">Recomeçar</button>';
 
 }
 
+/** Faz uma nova pergunta */
 function perguntar() {
 	curQuestao += 1;
 
+	// nao ha mais questoes disponiveis
 	if (curQuestao >= qttQuestao) {
 		encerrar();
 		return;
 	}
 
-	var conteudo = '<h2>' + (curQuestao+1) + ') ' + questoes[ordem[curQuestao]].pergunta + '</h2>';
-
-	if (questoes[ordem[curQuestao]].imagem != null)
-		conteudo += '<img src="' + questoes[ordem[curQuestao]].imagem + '">';
-
-	conteudo += '<br><form>';
-
+	// insere o conteudo da questao atual
 	var cur = questoes[ordem[curQuestao]];
+
+	var conteudo = '<h2>' + (curQuestao+1) + ') ' + cur.pergunta + '</h2>';
+	if (cur.imagem != null) // caso tenha imagem
+		conteudo += '<img src="' + cur.imagem + '">';
+
+	// opcoes de resposta
+	conteudo += '<br><form>';
 	for (var j = 0; j < cur.opcoes.length; j++) {
 		conteudo += '<div id="pergunta"><input type="radio" id="' + j + '" name="resp" value="' + cur.opcoes[j] + '">' + '<label for="' + j + '">' + cur.opcoes[j] + '</label></div>';
 	}
@@ -93,12 +106,13 @@ function perguntar() {
 	document.getElementById("questao").innerHTML = conteudo;
 }
 
+/** Compara a resposta do jogador com a resposta correta */
 function responder() {
 	var respondido = document.getElementById(questoes[ordem[curQuestao]].resposta).checked;
     var caixa = document.getElementById("container");
 
+    // compara as respostas e coloca um efeito
     setTimeout(function(){ caixa.className = ""}, 1000);
-
 	if (respondido) {
         caixa.className = "correct";
         pontos++;
@@ -106,7 +120,9 @@ function responder() {
         caixa.className = "wrong";
     }
     
+   	// atualiza a pontuacao
 	document.getElementById("pontuacao").innerHTML = "Pontos: " + pontos;
 
+	// nova pergunta
 	perguntar();
 }
